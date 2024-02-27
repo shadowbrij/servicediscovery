a39d8d6092332ed53de5cb70be84cad3ebdf0df3
Brijesh Kushwaha <bkushwaha89@gmail.com>
2024-02-27 16:15:32 +0530

    first commit servicediscovery

---
+++ src/test/java/dev/brijesh/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brijesh.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
